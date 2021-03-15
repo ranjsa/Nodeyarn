826de7795bdbc092fb1ecea4e9470fd4c8d41721
Tushar <gauravtushar7@gmail.com>
2021-03-15 08:21:30 +0530

    making default yes for cmd

--- installingNode.sh
+++ installingNode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-sudo apt install nodejs
-sudo apt install npm
+echo Y|sudo apt install nodejs
+echo Y|sudo apt install npm
 git clone https://github.com/ranjsa/Node_scripts.git
 cd Node_scripts
 node app.js
