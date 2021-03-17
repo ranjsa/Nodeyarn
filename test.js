d14104db6ef3e71a61f24a9220f94c4ce9c04f81
Tushar <gauravtushar7@gmail.com>
2021-03-17 12:47:24 +0530

    adding listr

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const cp = require('child_process');
 const chalk = require('chalk');
-const success = chalk.bold.green;
+const ora = require('ora');
 const Listr = require('listr');
+const success = chalk.bold.green;
 const info = chalk.bold.blue;
 const error = chalk.bold.red;
 const exec_options = {
@@ -13,6 +14,8 @@ const exec_options = {
     killSignal: "SIGTERM"
 };
 
+const spinner = ora(info("Take off Start")).start();
+
 const tasks = new Listr([
     {
         title: info('Install package'),
@@ -23,15 +26,49 @@ const tasks = new Listr([
             console.log(error(err));
         }}
     },
-    // {
-    //     title: 'Install package dependencies with npm',
-    //     skip: ctx => ctx.yarn !== false && 'Dependencies already installed with Yarn',
-    //     task: (ctx, task) => {
-    //         task.output = 'Installing dependencies...';
-
-    //         return execa('npm', ['install'])
-    //     }
-    // }
+    {
+        title: info('Installing curl and unzip'),
+        task: () => {try {
+            const installCurl = cp.execSync("sudo apt install curl", exec_options);
+            installCurl.toString();
+        } catch (err) {
+            console.log(error(err));
+        }}
+    },
+    {
+        title: info("Installing fnm"),
+        task: () => {try {
+            const install_fnm = cp.execSync("sudo curl -fsSL https://fnm.vercel.app/install | bash");
+            install_fnm.toString();
+        } catch (err) {
+            const ref_bash = cp.execSync("source ~/.bashrc", exec_options);
+            ref_bash.toString();
+            const install_fnm = cp.execSync("sudo curl -fsSL https://fnm.vercel.app/install | bash");
+            install_fnm.toString();
+            // console.log(error(err));
+        }}
+    },
+    {
+        title: info("Adding path"),
+        task: () => {try {
+            const reload_bashrc = cp.execSync(". ~/.bashrc");
+            reload_bashrc.toString();
+            console.log(info("Adding path"));
+            const config = cp.execSync("fnm completions --shell bash", exec_options);
+            config.toString();
+        } catch (err) {
+            console.log(error(err));
+        }}
+    },
+    {
+        title: info("Installing Node 14.16.0 LTS"),
+        task: () => {try {
+            const isntallNodeLTS = cp.execSync("fnm install 14.16.0", exec_options);
+            isntallNodeLTS.toString();
+        } catch (err) {
+            console.log(error(err));
+        }}
+    },
 ]);
 
 tasks.run().catch(err => {
