fcd99787555b4550ed6c79b74b931024d57b7bb8
Tushar <gauravtushar7@gmail.com>
2021-03-17 11:27:47 +0530

    DEBUG

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cp = require('child_process');
 const chalk = require('chalk');
 const success = chalk.bold.green;
 const info = chalk.bold.blue;
-//const error = chalk.bold.red;
+const error = chalk.bold.red;
 const exec_options = {
     cwd: null,
     env: null,
@@ -14,11 +14,20 @@ const exec_options = {
 };
 
 
-const spinner = ora(info("Install packages to allow apt to use a repository over HTTPS")).start();
+const spinner = ora(info("taking off")).start();
 // Install packages to allow apt to use a repository over HTTPS
 //console.log();
-const installPackg = cp.execSync("echo y|sudo apt-get install apt-transport-https ca-certificates curl software-properties-common", exec_options);
-installPackg.toString();
+try {
+    const spin1 = ora(info("Install packages to allow apt to use a repository over HTTPS")).start();
+    const installPackg = cp.execSync("echo y|sud apt-get install apt-transport-https ca-certificates curl software-properties-common", exec_options);
+    installPackg.toString();
+    spin1.prefixText = '✔️';
+    spin1.succeed(success("Installed  packages to allow apt to use a repository over HTTPS"));
+} catch (err) {
+    spin1.fail(error("err"));
+}
+spinner.succeed(info("top of the world!!"));
+
 //console.log(installPackg.toString());
-spinner.succeed(info("Install packages to allow apt to use a repository over HTTPS"));
-console.log("✔️" + success(" Done!"));
+
+//console.log("✔️" + success(" Done!"));
