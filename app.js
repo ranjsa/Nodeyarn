4e3ad9f987cf3beb21a5accaf3839ba650cc7b8a
Tushar <gauravtushar7@gmail.com>
2021-03-17 10:33:03 +0530

    Testing

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const ora = require('ora');
+const cp = require('child_process');
+const chalk = require('chalk');
+const ora = require('ora');
+const success = chalk.bold.green;
+const info = chalk.bold.blue;
+//const error = chalk.bold.red;
+const exec_options = {
+    cwd: null,
+    env: null,
+    encoding: 'utf8',
+    timeout: 0,
+    maxBuffer: 200 * 1024,
+    killSignal: "SIGTERM"
+};
+
 
 const spinner = ora('Loading unicorns').start();
-
-setTimeout(() => {
-    spinner.color = 'yellow';
-    spinner.text = 'Loading rainbows';
-}, 1000);
+const spinner = ora(info("Install packages to allow apt to use a repository over HTTPS")).start();
+// Install packages to allow apt to use a repository over HTTPS
+//console.log();
+const installPackg = cp.execSync("echo y|sudo apt-get install apt-transport-https ca-certificates curl software-properties-common", exec_options);
+console.log(installPackg.toString());
+spinner.stop();
+console.log("✔️" + success(" Done!"));
